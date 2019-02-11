4861abe072b5e9476a36e7ee41cea74a1f3496ea
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-02-10 20:10:04 -0800

    button click triggered by enter, changed div colors

--- assets/javascript/wiki.js
+++ assets/javascript/wiki.js
@@ -1,52 +1,48 @@
-
-        
-
 //Click function for youtube
-$(function(){
-    $('#search-btn').click(function(e){
-      e.preventDefault();
-      search();
+$(function () {
+    $('#search-btn').click(function (e) {
+        e.preventDefault();
+        search();
     });
-  
+
 });
-  
-  
-function search(){
+
+
+function search() {
     //clear results
     $('#results').html('');
     $('#buttons').html('');
     $('#bandPic').html('');
 
-  
+
     //Get Form Input
     q = $('#query').val();
-  
+
     wikiSearch(q);
-    
+
     wikiImages(q);
 
 };
 
 // This function pulls the main thumbnail from wikipedia
 
-function wikiImages (txt) {
+function wikiImages(txt) {
 
     $.ajax({
-      url: `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&wbptterms=description&gpssearch=${txt}&gpslimit=20`,
-      method: "GET",
-      dataType: "jsonp",
-      success: function(newData) {
+        url: `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&wbptterms=description&gpssearch=${txt}&gpslimit=20`,
+        method: "GET",
+        dataType: "jsonp",
+        success: function (newData) {
 
             $("#bandPic").html(`<img src='${newData.query.pages[0].thumbnail.source}' class='responsive-img valign'>`);
 
         }
-      }
-    )
-  };
+    })
+};
 
 // This function pulls the first paragraph from wikipedia
 
-function wikiSearch (txt) {
+function wikiSearch(txt) {
 
     $.ajax({
         type: "GET",
@@ -56,7 +52,7 @@ function wikiSearch (txt) {
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
             var firstText = data[2][0];
-                console.log(firstText);
+            console.log(firstText);
             $("#bandInfo").html(firstText);
 
         },
